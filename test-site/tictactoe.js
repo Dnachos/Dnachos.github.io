60dfb22a0414df912c35f9b6cebec8dccf5b2a8c
Dnachos <32440173+Dnachos@users.noreply.github.com>
2017-10-27 10:25:07 -0700

    Update tictactoe.js

--- test-site/tictactoe.js
+++ test-site/tictactoe.js
@@ -1,3 +1,10 @@
+"use strict";
+
+CanvasRenderingContext2D.prototype.circle = (_x, _y, _r) {
+  this.moveTo(_x + _r, _y);
+  this.arc(_x, _y, _r, 0, 2 * Math.PI);
+};
+
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
@@ -14,6 +21,5 @@ ctx.lineTo(300, 100);
 ctx.moveTo(0, 200);
 ctx.lineTo(300,200 );
 
-ctx.moveTo(100,50);
-ctx.arc(50,50,50,0, 2*Math.PI);
+ctx.circle(50, 50, 35);
 ctx.stroke();
