fa8526a1d1128ca5625445a67ca2a7f2a1380c3c
Dnachos <32440173+Dnachos@users.noreply.github.com>
2017-10-27 10:03:05 -0700

    Update tictactoe.js

--- test-site/tictactoe.js
+++ test-site/tictactoe.js
@@ -4,21 +4,16 @@ var ctx = canvas.getContext('2d');
 ctx.beginPath();
 ctx.moveTo(100, 0);
 ctx.lineTo(100, 300);
-ctx.stroke();
 
-
-ctx.beginPath();
 ctx.moveTo(200, 0);
 ctx.lineTo(200, 300);
-ctx.stroke();
 
-ctx.beginPath();
 ctx.moveTo(0, 100);
 ctx.lineTo(300, 100);
-ctx.stroke();
 
-ctx.beginPath();
 ctx.moveTo(0, 200);
 ctx.lineTo(300,200 );
-ctx.stroke();
 
+ctx.movetTo(100,50);
+ctx.arc(50,50,50,0, 2*Math.PI);
+ctx.stroke();
