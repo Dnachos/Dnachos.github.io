3da3f4038edde7401320a4030c606a9730fb60a5
Dnachos <32440173+Dnachos@users.noreply.github.com>
2018-02-15 20:41:09 -0800

    Add files via upload

--- test-site/blockEscape.js
+++ test-site/blockEscape.js
@@ -1,86 +1,21 @@
 "use strict";
-  var canvas;
-  const cols = 20;
-  const rows = 20;
-  const size = 30;
-  let loaded = false;
 
-  
-document.addEventListener("DOMContentLoaded", function() {
- 
-  
-  canvas = document.getElementById('canvas');
-     canvas.width = cols * size;
-     canvas.height = rows * size;
-
-    //let canvas = document.createElement("canvas");
-    //let ctx = canvas.getContext("2d");
-  menu();
- });
-
-
-
-
-
-
-
-function menu() {
-  if (loaded==false) {
-  canvas.addEventListener("click", function() {
-   console.log("click detected");
-   loaded = true;
-   classic();
-    });
-   }
- }
-   
-
-
-
-
-
-
-
-function classic() {
- 
-var ctx = canvas.getContext('2d');
-  
-  
-console.log("it works 1");
-  
-  
-    const shapes = [
-        [[1]],
-        [[0, 1], [0, 1], [1, 1]],
-        [[1, 1, 1]],
-        [[1, 1], [1, 0], [1, 1]],
-        [[1, 1], [1, 0]]
-    ];
-  
-  
-   class Vector {
+class Vector {
     constructor(_x, _y) {
         this.x = _x || 0;
-        this.y =  _y || 0;
-        
+        this.y = _y || 0;
+
     }
     add(obj) {
         this.x += obj.x;
         this.y += obj.y;
     }
-    set(x,y){
-        this.x=x;
-        this.y=y;
+    set(x, y) {
+        this.x = x;
+        this.y = y;
     }
-    
-} 
-  
-  
-   const min = new Vector(0, 0);
-    const max = new Vector(canvas.width, canvas.height);
-    const gravity = new Vector(0, 0.1);
-  
-  
+
+}
 class Piece extends Vector {
     constructor(_x, _y, _size, _solids) {
         super(_x, _y);
@@ -93,12 +28,11 @@ class Piece extends Vector {
             const col = this.spaces[x];
             for (let y = 0; y < col.length; y++) {
                 if (col[y]) {
-                   _ctx.rect(this.x + x * this.size, this.y + y * this.size, this.size, this.size);
+                    _ctx.rect(this.x + x * this.size, this.y + y * this.size, this.size, this.size);
                 }
             }
         }
     }
-   
     rotateCW() {
         const width = this.spaces.length;
         const height = this.spaces[0].length;
@@ -116,51 +50,57 @@ class Piece extends Vector {
     }
 }
 
+const cols = 20;
+const rows = 20;
+const size = 30;
+const gravity = new Vector(0, 0.1);
+const shapes = [
+    [
+        [1]
+    ],
+    [
+        [0, 1],
+        [0, 1],
+        [1, 1]
+    ],
+    [
+        [1, 1, 1]
+    ],
+    [
+        [1, 1],
+        [1, 0],
+        [1, 1]
+    ],
+    [
+        [1, 1],
+        [1, 0]
+    ]
+];
 
-console.log("it works 2");
-
-
-   
-    let ent = [];
-    window.entities = ent;
-    
-    let field = new Array(cols);
-    for (let x = 0; x < cols; x++) {
-        /*let col = field[x] = new Array(rows);
-        for (let y = 0; y < rows; y++) {
-            col[y] = 0;
-        }*/
-        field[x] = new Array(rows);
-    }
-
- console.log("it works 3");
-    
-    let tickNumber = 0;
-    (function loop() {
-        if (!(tickNumber % Math.floor(60 * 0.75))) { // ignore warning "Confusing use of '!'.".
-            const randShape = shapes[Math.floor(Math.random() * shapes.length)];
-            let newPiece = new Piece(0, 0, size, randShape);
-            const randRotation = Math.floor(Math.random() * 4);
-            for (let n = 0; n < randRotation; n++) {
-                newPiece.rotateCW();
-            }
-            newPiece.x = Math.floor(Math.random() * (cols - newPiece.spaces.length + 1)) * size;
-            newPiece.y = -newPiece.spaces[0].length * size;
-            newPiece.color = "hsl(" + Math.floor(Math.random() * 360) + ", 100%, 50%)";
-            ent.push(newPiece)
+const loop = () => {
+    if (!(tickNumber % Math.floor(60 * 0.75))) {
+        const randShape = shapes[Math.floor(Math.random() * shapes.length)];
+        let newPiece = new Piece(0, 0, size, randShape);
+        const randRotation = Math.floor(Math.random() * 4);
+        for (let n = 0; n < randRotation; n++) {
+            newPiece.rotateCW();
         }
-        ctx.fillStyle = "#000";
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-        entLoop:
+        newPiece.x = Math.floor(Math.random() * (cols - newPiece.spaces.length + 1)) * size;
+        newPiece.y = -newPiece.spaces[0].length * size;
+        newPiece.color = "hsl(" + Math.floor(Math.random() * 360) + ", 100%, 50%)";
+        ent.push(newPiece)
+    }
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    entLoop:
         for (let n = 0; n < ent.length; n++) {
             let cur = ent[n];
             cur.velocity.add(gravity);
             cur.add(cur.velocity);
-            //cur.constrain(min, max);
             let height = cur.spaces[0].length;
-            for(let x = 0; x < cur.spaces.length; x++) {
+            for (let x = 0; x < cur.spaces.length; x++) {
                 const col = cur.spaces[x];
-                for(let y = height - 1; y >= 0; y--) {
+                for (let y = height - 1; y >= 0; y--) {
                     if (col[y]) {
                         const curX = cur.x / size + x;
                         const curY = Math.floor(cur.y / size) + y + 1;
@@ -184,22 +124,48 @@ console.log("it works 2");
             cur.draw(ctx);
             ctx.fill();
         }
-        for (let x = 0; x < cols; x++) {
-            const col = field[x];
-            for (let y = 0; y < rows; y++) {
-                const cur = col[y];
-                if (cur) {
-                    ctx.fillStyle = cur;
-                    ctx.fillRect(x * size, y * size, size, size);
-                }
+    for (let x = 0; x < cols; x++) {
+        const col = field[x];
+        for (let y = 0; y < rows; y++) {
+            const cur = col[y];
+            if (cur) {
+                ctx.fillStyle = cur;
+                ctx.fillRect(x * size, y * size, size, size);
             }
         }
-        tickNumber++;
-        requestAnimationFrame(loop);
-    })();
-    
+    }
+    tickNumber++;
+    requestAnimationFrame(loop);
+};
+const classic = () => {
+    field = new Array(cols);
+    for (let x = 0; x < cols; x++) {
+        field[x] = new Array(rows);
+    }
+    requestAnimationFrame(loop);
+};
 
-}
+let ent = [];
+let canvas;
+let ctx;
+let tickNumber = 0;
+let field;
 
+document.addEventListener("DOMContentLoaded", () => {
+    canvas = document.getElementById("canvas");
+    canvas.width = cols * size;
+    canvas.height = rows * size;
+    ctx = canvas.getContext("2d");
 
+    ctx.fillStyle = "rgb(0,0,0)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+    ctx.beginPath();
+    ctx.strokeStyle = "white";
+    ctx.rect(150,150,200,50);
+    ctx.stroke();
+    ctx.strokeStyle = "white";
+    ctx.strokeText("Classic", 200,200);
+    ctx.font = "36px crayon";
+    canvas.addEventListener("click", classic, {once: true});
+});
