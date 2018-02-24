295da7d622e13a0b94f8760f5bd7f5b7f2f70c86
Dnachos <32440173+Dnachos@users.noreply.github.com>
2018-02-23 16:15:11 -0800

    Add files via upload

---
+++ test-site/scripts/main.js
@@ -0,0 +1,34 @@
+let myImage;
+let ticButton;
+let domLoaded = false;
+let fontLoaded = false;
+let font = new FontFace("Crayon", "url(fonts/crayonFont.ttf)");
+document.fonts.add(font);
+font.load().then(() => {
+	fontLoaded = true;
+	if (domLoaded) {
+		begin();
+	}
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+	domLoaded = true;
+	if (fontLoaded) {
+		begin();
+	}
+	myImage = document.getElementById("myImage");
+	myImage.addEventListener("click", imageClicked);
+	ticButton = document.getElementById("ticButton");
+	ticButton.addEventListener("click", () => {
+		open("tictactoe.html", "TICTACTOE");
+	});
+});
+
+const imageClicked = () => {
+	if (myImage.src.includes("images/apple.jpg")) {
+		alert("NOOOOOO!!!!!");
+		myImage.src = "images/appleCore.jpg";
+	} else {
+		myImage.src = "images/apple.jpg";
+	}
+};
