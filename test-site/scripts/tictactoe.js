7ebb652bdf9284c80a20d6b6ad4d8cd95f7ed900
Dnachos <32440173+Dnachos@users.noreply.github.com>
2018-02-26 14:39:59 -0800

    Update tictactoe.js

--- test-site/scripts/tictactoe.js
+++ test-site/scripts/tictactoe.js
@@ -105,8 +105,8 @@ if (turn == 9 && win ==false) {
 				
 				if (topWin == true) {
 					ctx.beginPath();
-					ctx.moveTo(0, 150);
-					ctx.lineTo(size * complexity, 150);
+					ctx.moveTo(0, 100);
+					ctx.lineTo(size * complexity, 100);
 					ctx.lineWidth = 3;
 					ctx.stroke();
 				}
