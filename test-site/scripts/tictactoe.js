7018ce95ffb0485e9ffee37bd162b4d9dcc51ccc
Dnachos <32440173+Dnachos@users.noreply.github.com>
2018-03-03 10:49:38 -0800

    Update tictactoe.js

--- test-site/scripts/tictactoe.js
+++ test-site/scripts/tictactoe.js
@@ -51,7 +51,7 @@ const canvasClicked = event => {
 		
 		
 			for (let n = 0; n < complexity; n++) {
-				if (toeField[0][0].wok && toeField[1][0].wok && toeField[2][0].wok == toeField[0][0].wok) {
+				if (toeField[0][0].wok == toeField[1][0].wok && toeField[2][0].wok == toeField[1][0].wok) {
 					ctx.beginPath();
 					ctx.moveTo(0, 100);
 					ctx.lineTo(600,100);
