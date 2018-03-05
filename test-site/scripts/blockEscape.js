117042faf058b8bdf719cb0b987e72699a6ac1f3
Dnachos <32440173+Dnachos@users.noreply.github.com>
2018-03-05 17:46:33 +0000

    Update blockEscape.js

--- test-site/scripts/blockEscape.js
+++ test-site/scripts/blockEscape.js
@@ -170,7 +170,7 @@ const begin = () => {
 	}, 33);
 
 	
-	let button = document.canvas.createElement("button");
+	let button = document.createElement("button");
 				button.style.width = "150px";
 				button.style.height = "50px";
 				button.style.position = "absolute";
