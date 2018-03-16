1d671afa55faeafe6095f3804b371bc433a64029
Dnachos <32440173+Dnachos@users.noreply.github.com>
2018-03-16 09:36:06 -0700

    Update blockEscape.js

--- test-site/scripts/blockEscape.js
+++ test-site/scripts/blockEscape.js
@@ -49,8 +49,8 @@ class Piece extends Vector {
 	}
 }
 
-const cols = 22;
-const rows = 23;
+const cols = 23;
+const rows = 22;
 const size = 35;
 const gravity = new Vector(0, 0.1);
 const shapes = [
